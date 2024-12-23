cf464866ec25af3d505da9ee05728ae9b21985dd
Mateusz Jaros <franekjaros@gmail.com>
2024-12-24 00:26:19 +0100

    dodanie opcji aby generowane url byly po https

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'nobodywilleverknow'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Configure the app to use HTTPS as the preferred URL scheme
+app.config['PREFERRED_URL_SCHEME'] = 'https'
 
 
 db = SQLAlchemy(app)
